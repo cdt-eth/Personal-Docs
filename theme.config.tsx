e0e853ec5787ac75f728eda95decb16b92567a5b
christianturner <christianturner@hey.com>
2023-10-09 10:05:58 -0400

    change github link

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>cdt docs</span>,
+  logo: <span>cdts docs</span>,
   project: {
     link: 'https://github.com/cdt-eth',
   },
-faviconGlyph: '🆒',
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  faviconGlyph: '🆒',
+  docsRepositoryBase: 'https://github.com/cdt-eth',
   footer: {
     text: 'made by cdt.',
-  }, 
-}
+  },
+};
 
-export default config
+export default config;
